5349ab984f3c1de4471e4606c5aa677743a867a1
JeetDPatel11 <pateljeet1118@gmail.com>
2026-03-13 13:12:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_community.llms import Ollama
+import streamlit as st
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+# Langsmith Tracking
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["LANGSMITH_PROJECT"] = os.getenv("LANGSMITH_PROJECT")
+
+# Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+    ("system","You are a helpful assistant. Please respond to the question asked"),
+    ("user","Question:{question}")
+    ]
+)
+
+
+# streamlit framework
+st.title("JDPA11")
+input_text = st.text_input("What question you have in mind?")
+
+# Ollama gemma3 model
+llm = Ollama(model="gemma3:1b")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+
